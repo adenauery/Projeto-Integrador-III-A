8ee7fbfff8a98b1f780bc8448125830eb9924101
Adenauer Yamin <adenauer@gmail.com>
2022-04-03 14:11:41 -0300

    Update publica-emqx-cron.sh

--- publica-emqx-cron.sh
+++ publica-emqx-cron.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Este script Bash publica via MQTT o valor gravado em arquivo pelo outro script publica-emqx.sh
+# Abaixo o comando de crontab utilizado:
+# 0-59/1 * * * * /home/adenauer/pi-3-a/publica-emqx-cron.sh
 
 CPU_USAGE=`(cat /home/adenauer/pi-3-a/ultimacpu.txt)`
 echo $CPU_USAGE
